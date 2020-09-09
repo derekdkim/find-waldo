fbebf28c84f0de80dd5c0c5689e849e9ee233c97
Derek Kim <ddkim1024@gmail.com>
2020-09-09 17:17:21 -0500

    Project setup with create-react-app and Firebase. Added photo hosted from Firebase server

---
+++ src/components/Photo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Photo extends React.Component {
+  render() {
+    return (
+      <img 
+        src='https://firebasestorage.googleapis.com/v0/b/find-waldo-8be9b.appspot.com/o/WheresWallyAtWembley_6.jpg?alt=media&token=bc667aaf-ae09-476b-b226-8a4d6b8207c5'
+        alt='Waldo'
+      />
+    );
+  }
+}
+
+export default Photo;
